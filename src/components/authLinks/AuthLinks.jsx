3e507ec1e97ca26b4fd6bf7b204ff1407ec1e086
Rageh97 <abdelhady412rageh@gmail.com>
2023-10-03 17:02:32 +0300

    setup folder strucure and responsive

---
+++ src/components/authLinks/AuthLinks.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import styles from "./authLinks.module.css"
+const AuthLinks = () => {
+  return (
+    <div className={styles.container}>
+      
+    </div>
+  )
+}
+
+export default AuthLinks

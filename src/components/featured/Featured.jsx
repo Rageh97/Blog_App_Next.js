f15c7e7765b499e90834c17df2aacf293d9a25e8
Rageh97 <abdelhady412rageh@gmail.com>
2023-10-05 16:39:46 +0300

    Add featured header

--- src/components/featured/Featured.jsx
+++ src/components/featured/Featured.jsx
@@ -1,9 +1,27 @@
-import React from 'react'
-import styles from "./featured.module.css"
+import React from "react";
+import styles from "./featured.module.css";
+import Image from "next/image";
 const Featured = () => {
   return (
-    <div className={styles.container}>Navbar</div>
-  )
-}
+    <div className={styles.container}>
+      <h1 className={styles.title}>
+        <b >Hey, Mohamed Rageh</b> Discover The Varient Blogs here
+      </h1>
+      <div className={styles.post}>
+        <div className={styles.imgContainer}>
+          <Image className={styles.image} src="/p1.jpeg" alt="" fill />
+        </div>
+        <div className={styles.textContainer}>
+          <h1 className={styles.postTitle}>Lorem ipsum, dolor sit amet consectetur adipisicing.</h1>
+          <p className={styles.postDesc}>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Incidunt
+            ducimus voluptatibus recusandae doloremque, dolorem cupiditate?
+          </p>
+          <button className={styles.button}>Read More</button>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default Featured
+export default Featured;

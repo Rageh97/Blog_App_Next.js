04d518f4b48de3ed0d85d971dc149d88daa244f2
Rageh97 <abdelhady412rageh@gmail.com>
2023-10-07 17:14:27 +0300

    Add cardlist and card posts

---
+++ src/components/card/Card.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+import styles from "./card.module.css";
+import Image from "next/image";
+import Link from "next/link";
+const Card = () => {
+  return (
+    <div className={styles.container}>
+      <div className={styles.imageContainer}>
+        <Image src="/p1.jpeg" alt="" fill className={styles.image}/>
+      </div>
+      <div className={styles.textContainer}>
+        <div className={styles.detail}>
+          <span className={styles.date}>11.02.2023</span>
+          <span className={styles.category}>Culture</span>
+        </div>
+        <Link href="">
+          <h1>Lorem ipsum dolor sit amet consectetur adipisicing.</h1>
+        </Link>
+        <p className={styles.desc}>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis nemo
+          quam iusto similique cum nesciunt voluptatem. Inventore incidunt eos
+          ipsum deleniti. Minima dicta, nesciunt itaque veniam eius sapiente
+          incidunt vel!
+        </p>
+        <Link className={styles.link} href="">Read More</Link>
+      </div>
+    </div>
+  );
+};
+
+export default Card;
